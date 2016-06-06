23f5d6fd6170dbd66e768928bf84d88b181a9221
Abbas Oveissi <abbas.oveissi@gmail.com>
2016-06-06 17:39:10 +0430

    ezafe kardan e endless scroll

--- app/src/main/java/ir/oveissi/simpleretrofitsample/activities/MoviesActivity.java
+++ app/src/main/java/ir/oveissi/simpleretrofitsample/activities/MoviesActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-
 import java.util.ArrayList;
 
 import ir.oveissi.simpleretrofitsample.R;
@@ -16,6 +14,7 @@ import ir.oveissi.simpleretrofitsample.data.ApiClient;
 import ir.oveissi.simpleretrofitsample.data.ApiInterface;
 import ir.oveissi.simpleretrofitsample.data.jsonmodel.Movie;
 import ir.oveissi.simpleretrofitsample.data.jsonmodel.TmpMovies;
+import ir.oveissi.simpleretrofitsample.utils.EndlessRecyclerOnScrollListener;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,13 +32,19 @@ public class MoviesActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this));
         mListAdapter=new MoviesAdapter(this, new ArrayList<Movie>());
         rv.setAdapter(mListAdapter);
-        get_data_from_webservice();
+        rv.addOnScrollListener(new EndlessRecyclerOnScrollListener((LinearLayoutManager) rv.getLayoutManager()) {
+            @Override
+            public void onLoadMore(int current_page) {
+                get_data_from_webservice(current_page);
+            }
+        });
+        get_data_from_webservice(1);
     }
 
-    private void get_data_from_webservice() {
+    private void get_data_from_webservice(int page) {
 
         Call<TmpMovies> call=
-                ApiClient.getClient().create(ApiInterface.class).getMoviesByTitle("batman",1);
+                ApiClient.getClient().create(ApiInterface.class).getMoviesByTitle("batman",page);
         call.enqueue(new Callback<TmpMovies>() {
             @Override
             public void onResponse(Call<TmpMovies> call, Response<TmpMovies> response) {
